78fe26838ad316f10c0c2ea09bd161d8281c27ba
danielsun <danielsun12345@gmail.com>
2020-11-02 20:53:07 -0800

    installed packages and imported packages to index

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/accounts/1/boba', {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="App">
